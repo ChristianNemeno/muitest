49a69af6cdbe99d337d1053e4980e0b954876123
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-07-29 21:52:09 +0800

    sleep

--- my-app/src/pages/MuiSamplePage.jsx
+++ my-app/src/pages/MuiSamplePage.jsx
@@ -1,14 +1,19 @@
 ``
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Container, Card, CardContent, CardActions, Grid, Stack } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Container, Card, CardContent, CardActions, Grid, Stack , Divider} from '@mui/material';
 
 function MuiSamplePage() {
   return (
 
+    <Stack direction="row" spacing={2} divider={<Divider orientation="vertical" flexItem />}>
+
+    <Container maxWidth="sm" >Question</Container>
     <Stack direction ="row" spacing = {2}>
       <Button variant='contained'>Option 1</Button>
       <Button variant='contained'>Option 2</Button>
       <Button variant='contained'>Option 3</Button>
+    </Stack>
+
     </Stack>
   );
 }
